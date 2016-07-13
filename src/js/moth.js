f2dc060c498b865bff4e9d5a249c8b3517dd9d1e
kevronosx <kevin@joinhomebase.com>
2016-07-13 16:03:25 -0700

    change click target for animation trigger

--- src/js/moth.js
+++ src/js/moth.js
@@ -10,30 +10,37 @@ export default class Moth {
     this.stage = document.querySelector(".stage");
     this.stage.style.display = "block";
     this.stalk = document.querySelector(".stage--center-stalk");
+    this.rightLeaf = document.querySelector(".stage--right-leaf");
     this.audio = document.querySelector(".flute-audio");
     this.play();
     this.events();
   }
 
   events() {
-    this.stalk.addEventListener('click', this.play.bind(this), false);
-    this.audio.addEventListener('ended', this.musicCompleted.bind(this), false);
+    this.stage.addEventListener('click', this.play.bind(this), false);
     this.stalk.addEventListener("animationend", this.stalkCompleted.bind(this), false);
-  }
-
-  stalkCompleted() {
-    this.stalk.classList.add('pulse');
-  }
-
-  musicCompleted(e) {
-    this.audio.currentTime = 0;
-    this.stage.classList.remove('start');
-    this.stalk.classList.remove('pulse');
+    this.rightLeaf.addEventListener("animationend", this.leafCompleted.bind(this), false);
+    this.audio.addEventListener('ended', this.musicCompleted.bind(this), false);
   }
 
   play() {
     this.stage.classList.add("start");
     this.audio.volume = 0.1;
     this.audio.play();
+  }
+
+  stalkCompleted() {
+    this.stalk.classList.add('pulse');
+  }
+
+  leafCompleted() {
+    setTimeout(() => {
+      this.stage.classList.remove('start');
+      this.stalk.classList.remove('pulse');
+    }, 500);
+  }
+
+  musicCompleted(e) {
+    this.audio.currentTime = 0;
   }
 }
